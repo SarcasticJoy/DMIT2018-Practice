4658cbad5b73a75238fbf4a84b67622e05f8b9a8
SarcasticJoy <64291442+SarcasticJoy@users.noreply.github.com>
2021-05-07 18:53:36 -0600

    May7 Additional files

---
+++ ChinookSystem/Entities/Album.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Album")]
+    internal class Album
+    {
+        [Key]
+        public int AlbumId { get; set; } //PK
+        [Required(ErrorMessage = "Title required")]
+        [StringLength(160, ErrorMessage = "Album title is limited to 160 characters")]
+        public string Title { get; set; }
+        public int ArtistId { get; set; } //FK [ForeignKey] only name if name is different. May create problems if yu add 
+        public int ReleaseYear { get; set; }
+        [StringLength(50, ErrorMessage = "ReleaseLabel is limited to 50 characters")]
+        public string ReleaseLabel { get; set; }
+    }
+}
---
+++ ChinookSystem/Entities/Artist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Artist")]
+    internal class Artist // Internal to keep secure. Unlike last DMIT class.
+    {
+        // [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Tells the system not to guess the key but to generate it, and with an identity # generated. 
+        [Key]
+        public int ArtistId { get; set; }
+
+        // assessor is the get , mutator is the set
+
+        [Required(ErrorMessage = "Artist Name is required.")]
+        [StringLength(120, MinimumLength = 1, ErrorMessage = "Artist name is limited to 120 characters")] // max, then min but you need to specifiy the min
+        public string Name { get; set; }
+
+    }   // If the names are the same as the database, they will auto line up. If the names differ, you will have to type them in the correct order yourself.
+}
